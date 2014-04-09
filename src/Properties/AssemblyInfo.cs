31b54ee3fc3a34032dad962dff5d19bcf53763d4
rofr <robert@devrex.se>
2014-04-09 10:56:17 +0200

    initial code checkin, not fully implemented
    Next steps, adding unimplemented methods and unit tests

---
+++ src/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("OrigoDB.Models.Redis")]
+[assembly: AssemblyDescription("Redis clone for OrigoDB")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Devrex Labs")]
+[assembly: AssemblyProduct("OrigoDB.Models.Redis.Properties")]
+[assembly: AssemblyCopyright("Devrex Labs")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("13a53659-267d-4a87-ba06-f1771209aa17")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("0.1")]
+[assembly: AssemblyFileVersion("0.1")]
